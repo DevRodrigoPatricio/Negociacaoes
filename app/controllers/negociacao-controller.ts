159eec614c3acb693792baf5762931620e67c185
RodrigoPatricio60 <rodrigopatricio19@gmail.com>
2022-12-29 16:35:24 -0300

    estudos em typescript

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -9,19 +9,25 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
    
 
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
-        this.negociacoesView.update(this.negociacoes);
+        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
+        this.negociacoesView.update(this.negociacoes) ;
     }
 
+
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
+
+        const negociacao = Negociacao.criarDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView
                 .update('Apenas negociações em dias úteis são aceitas');
@@ -38,13 +44,8 @@ export class NegociacaoController {
             && data.getDay() < DiasDaSemana.SABADO;
     }
 
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
-    }
+    
+
 
     private limparFormulario(): void {
         this.inputData.value = '';
